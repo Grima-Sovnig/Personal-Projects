0cc6e260ab7ff61c16baac9c36198420a296d225
Grïmą <paradoxderp65@gmail.com>
2022-03-10 02:37:15 -0600

    Mac Commit

---
+++ C++/HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Gabriel Snider on 3/10/22.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    return 0;
+}
