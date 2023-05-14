451bbdbd73653019442feecebdb774b5a6603208
Grïmą <paradoxderp65@gmail.com>
2023-05-14 12:53:17 -0500

    Start of Program
    Added Comments

---
+++ MindTransferral/MindTransferral.py
@@ -0,0 +1,6 @@
+## This is the driver file for the main program.
+## This program is malicious by nature, do not run unless you intend to cause damage or understand what you are doing.
+## I am not responsible for any damage or trouble you may get into by running this program.
+
+## The intent of this program is to steal browser password stores and copy the decrypted data to a remote server.
+
